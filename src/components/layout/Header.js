44e1033bd8a3973fe4d38f7d70eaa3733be17fbc
Hamir Mahal <hamirmahal@gmail.com>
2020-07-03 06:56:50 -0700

    Made About.js component reachable from Header.js

--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -1,10 +1,18 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 export default function Header() {
     return (
         <h1 style   =   {   {   padding:    '10px'  }   }>
             TodoList from
-            Traversy Media React JS Crash Course
+            Traversy Media React JS Crash Course <br />
+            <Link style={homeStyle} to='/'>Home</Link>
+            {''} | {''}
+            <Link style={homeStyle} to='/about'>About</Link>
         </h1>
     );
+}
+
+const homeStyle = {
+    fontSize: '75%',
 }
